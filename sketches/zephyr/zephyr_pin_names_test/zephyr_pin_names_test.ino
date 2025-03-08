555f1289e9eea9f9cb0d27e512effab9f33266b5
kurte <kurte@rockisland.com>
2025-03-08 12:05:40 -0800

    Update zephyr_pin_names_test.ino
    quick and dirty test for PinName on digitalRead

--- sketches/zephyr/zephyr_pin_names_test/zephyr_pin_names_test.ino
+++ sketches/zephyr/zephyr_pin_names_test/zephyr_pin_names_test.ino
@@ -23,16 +23,27 @@ void setup() {
       }
     }
   }
+  pinMode(PJ_11, INPUT_PULLUP);
+
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_BLUE, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);
 }
 
 uint8_t loop_count = 0;
+PinStatus pj11_state_prev = HIGH;
 void loop() {
   loop_count = (loop_count + 1) & 0x7;
   digitalWrite(LED_RED, (loop_count & 1) ? HIGH : LOW);
   digitalWrite(LED_BLUE, (loop_count & 2) ? HIGH : LOW);
   digitalWrite(LED_GREEN, (loop_count & 4) ? HIGH : LOW);
+#if 1
+  PinStatus pj11_state = digitalRead(PJ_11);
+  if (pj11_state != pj11_state_prev) {
+    pj11_state_prev = pj11_state;
+    Serial.print("PJ_11(D2): ");
+    Serial.println(pj11_state? "HIGH" : "LOW");
+  }
+  #endif
   delay(250);
 }
