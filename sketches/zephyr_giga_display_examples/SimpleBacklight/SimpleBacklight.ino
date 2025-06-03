9bda40d438f3b40f2cddf827c3b41359b42b58eb
kurte <kurte@rockisland.com>
2025-06-02 17:48:50 -0700

    playing with backlight

---
+++ sketches/zephyr_giga_display_examples/SimpleBacklight/SimpleBacklight.ino
@@ -0,0 +1,21 @@
+/*
+  Changes the display backlight from very dim to 100%
+  This example code is in the public domain.
+*/
+
+#include "Arduino_GigaDisplay.h"
+#include "GigaDisplayRGB.h"
+
+//Create backlight object
+GigaDisplayBacklight backlight;
+
+void setup() {
+  //init library
+  backlight.begin();
+}
+
+int i = 0;
+void loop() {
+  backlight.set(i++ % 100);
+  delay(100);
+}
