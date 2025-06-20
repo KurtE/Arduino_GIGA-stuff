5fb6f9c9438a7b05fb83a8382eb8f553564c5407
Kurt Eckhardt <kurte@rockisland.com>
2025-06-20 10:23:30 -0700

    Update TFT_String_Align.ino

--- libraries/ST77XX_zephyr/examples/TFT_String_Align/TFT_String_Align.ino
+++ libraries/ST77XX_zephyr/examples/TFT_String_Align/TFT_String_Align.ino
@@ -15,26 +15,48 @@ BC_DATUM = Bottom centre
 BR_DATUM = Bottom right
 */
 #include <SPI.h>
-#include <ST7735_zephyr.h> // Hardware-specific library
-#include <ST7789_zephyr.h> // Hardware-specific library
-#include <st7735_t3_font_Arial.h>
+#include <ST77XX_zephyr.h>
+#include <ST77XX_zephyr_font_Arial.h>
 
 
-#define TFT_MISO  1
-#define TFT_MOSI  26  //a12
-#define TFT_SCK   27  //a13
-#define TFT_DC   6 
-#define TFT_CS   0  
-#define TFT_RST  5
+#define TFT_CS   10  // CS & DC can use pins 2, 6, 9, 10, 15, 20, 21, 22, 23
+#define TFT_DC    8  //  but certain pairs must NOT be used: 2+10, 6+9, 20+23, 21+22
+#define TFT_RST   9  // RST can use any pin
 
-ST7789_t3 tft = ST7789_t3(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCK, TFT_RST);
+// Use one or the other
+//ST7735_zephyr tft = ST7735_zephyr(&SPI, TFT_CS, TFT_DC, TFT_RST);
 
+// For 1.54" TFT with ST7789
+//ST7789_zephyr tft = ST7789_zephyr(&SPI, TFT_CS, TFT_DC, TFT_RST);
+
+// For 3.5" or 4.0" TFT with ST7796
+ST7796_zephyr tft = ST7796_zephyr(&SPI1, TFT_CS, TFT_DC, TFT_RST);
 unsigned long drawTime = 0;
 
 void setup(void) {
   Serial.begin(115200);
   SPI.begin();
-  tft.init(240, 320);
+  // Use this initializer if you're using a 1.8" TFT 128x160 displays
+  //tft.initR(INITR_BLACKTAB);
+
+  // Or use this initializer (uncomment) if you're using a 1.44" TFT (128x128)
+  //tft.initR(INITR_144GREENTAB);
+
+  // Or use this initializer (uncomment) if you're using a .96" TFT(160x80)
+  //tft.initR(INITR_MINI160x80);
+
+  // Or use this initializer (uncomment) for Some 1.44" displays use different memory offsets
+  // Try it if yours is not working properly
+  // May need to tweek the offsets
+  //tft.setRowColStart(32,0);
+
+  // Or use this initializer (uncomment) if you're using a 1.54" 240x240 TFT
+  //tft.init(240, 240);   // initialize a ST7789 chip, 240x240 pixels
+
+  // OR use this initializer (uncomment) if using a 2.0" 320x240 TFT:
+  //tft.init(240, 320);           // Init ST7789 320x240
+  tft.init(320, 480);
+
   tft.setRotation(3);
   tft.setFont(Arial_18);
   //tft.setTextSize(4);
